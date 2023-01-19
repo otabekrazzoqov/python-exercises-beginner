4a7000760f015d3e450f41b411cd01ea09c62b26
otabekrazzoqov <razzoqovotabek1@gmail.com>
2023-01-19 21:56:42 +0900

    updated

--- hello-world.py
+++ hello-world.py
@@ -414,7 +414,7 @@
 
 #LISTS
 
-cars = ['lacetti', 'bugatti', 'bmw', 'mercedes-benz', 'tesla', 'tiko' , 'audi']
+#cars = ['lacetti', 'bugatti', 'bmw', 'mercedes-benz', 'tesla', 'tiko' , 'audi']
 # .sort() metodi ro'yxatdagi so'zlarni alifbo tartibida joylashtiradi.
 #cars[0].capitalize()
 #cars.sort(reverse=True)
@@ -490,96 +490,242 @@ cars = ['lacetti', 'bugatti', 'bmw', 'mercedes-benz', 'tesla', 'tiko' , 'audi']
 
 
 
-toys = ("car", "lizard", "bear", "cat", "doggo") #() tuple o'zgarmas ro'yxat
+#toys = ("car", "lizard", "bear", "cat", "doggo") #() tuple o'zgarmas ro'yxat
 # tuple o'zgarmas ro'yxatga element qo'shib bo'lmaydi. tuple ni list ga 
 #almashtirish yo'li bilan ro'yxatga element qo'shish mumkin.
 
-toys = list(toys)
-toys.append("chumoli") # .append - ro'yxatga element qo'shish
-toys.append('tuya')
+#toys = list(toys)
+#toys.append("chumoli") # .append - ro'yxatga element qo'shish
+#toys.append('tuya')
 
-print(toys)
-toys = tuple(toys)
-print(toys)
+#print(toys)
+#toys = tuple(toys)
+#print(toys)
 
-
-
-
+#HOMEWORK
 
 
-#JUNE 20. 2022****************************************************************
+# O'zingizga ma'lum davlatlarning ro'yxatini tuzing va ro'yxatni 
+#konsolga chiqaring
+
+#davlatlar = ["O'zbekiston", "Qozog'iston", "Rossiya", "Malayziya", "Singapur", "AQSh"]
+#print(davlatlar)
+
+#Ro'yxatning uzunligini konsolga chiqaring
+
+#print(len(davlatlar))
+
+
+# sorted() funktsiyasi yordamida ro'yxatni tartiblangan holda konsolga chiqaring
+#print(sorted(davlatlar))
+
+
+# sorted() yordamida ro'yxatni teskari tartibda konsolga chiqaring
+
+#print(sorted(davlatlar, reverse=True))
+
+
+# Asl ro'yxatni qaytadan konsolga chiqaring
+
+#print(davlatlar)
+
+
+# reverse() metodi yordamida ro'yxatni ortidan boshlab chiqaring
+
+#davlatlar.reverse()
+#print(davlatlar)
+
+#sort() metodi yordamida ro'yxatni avval alifbo bo'yicha, keyin esa 
+#alifboga teskari tartibda konsolga chiqaring.
+
+#davlatlar.sort()
+#print(davlatlar)
+
+#davlatlar.sort(reverse=True)
+#print(davlatlar)
+
+#120 dan 1200 gacha bo'lgan juft sonlar ro'yxatini tuzing
+
+#sonlar = list(range(120,1200))
+
+
+# Ro'yxatdagi sonlar yig'indisini hisoblang va konsolga chiqaring
+
+#print(sum(sonlar))
+
+#print(max(sonlar)-min(sonlar))
+# Ro'yxatdagi eng katta va eng kichik son o'rtasidagi ayirmani hisoblang 
+#va konsolga chiqaring
+
+#print(max(sonlar) - min(sonlar))
+
+# Ro'yxatdagi elementlar sonini hisoblang
+
+#print(len(sonlar))
+
+# Ro'yxatning boshidan, o'rtasidan va oxiridan 20 ta qiymatni konsolga chiqaring
+
+#print(sonlar[:20])
+#print(sonlar[-20:])
+#print(sonlar[530:550])
+
+
+#taomlar degan ro'yxat yarating va ichiga istalgan 5ta taomni kiriting
+
+#taomlar = ['osh','somsa','norin','shashlik','qozonkabob']
+#print(taomlar)
+#nonushta degan yangi ro'yxatga taomlardan nusxa oling
+
+#nonushta = taomlar[:]
+
+#Yangi ro'yxatda faqat nonushtaga yeyiladigan taomlarni qoldiring, 
+#va qo'shimcha 2 ta taom qo'shing
+
+#nonushta.remove('norin')
+#nonushta.remove('shashlik')
+#nonushta.remove('qozonkabob')
+#nonushta.append('non va qaymoq')
+#nonushta.append('issiq non')
+
+#print(nonushta)
 
 
 
 
-#mehmonlar = ['ali' , 'vali' , 'hasan' , 'husan' , 'olim']
-#for mehmon in mehmonlar:
+# Ikkala ro'yxatni ham (taomlar va nonushta) konsolga chiqaring
+
+#print(taomlar)
+#print(nonushta)
+
+# Yuqoridagi nonushta ro'yxatini o'zgarmas ro'yxatga 
+#aylantiring va nonushta[0] = "qaymoq va non" deb qiymat berib ko'ring.
+
+#nonushta = tuple(nonushta)
+#nonushta[0] = "qaymoq va non"
+#nonushta[0] = "qaymoq va non"
+
+
+
+
+
+
+#***************************** DARS 8 ********************
+
+
+
+
+mehmonlar = ['ali' , 'vali' , 'hasan' , 'husan' , 'olim']
+#print('salom', mehmonlar[1])
+#print('salom', mehmonlar[0])
+#print('salom', mehmonlar[2])
+
+
+#for mehmon in mehmonlar:  # for operatori kodni qayta qayta takrorlaydi 
+#mehmonlar ichidagi har bir mehmon uchun
+
 #    print("salom", mehmon)
 #    print("hayr", mehmon)
 
 
 #mehmonlar = ['ali' , 'vali' , 'hasan' , 'husan' , 'olim']
 #for mehmon in mehmonlar:
-#    print(f"Hurmatli {mehmon} sizni 11 avgust kuni majlisga taklif etamiz")
-#    print("hurmat bilan thompsonlar oilasi\n")
+#    print(f"Hurmatli {mehmon} sizni 24 avgust kuni majlisga taklif etamiz")
+#    print("hurmat bilan Thompsonlar oilasi\n")
+    
+    
     
     
 
-#sonlar = list(range(1,11))
+#sonlar = list(range(1,1001,10))
 #for son in sonlar:
 #    print(f"{son} ning kvadrati {son**2} ga teng")
+#    print("understood?")
 
 
 
 
-#sonlar = list(range(11))
-#sonlar_kvadrati = []
-#for son in sonlar:
-#    sonlar_kvadrati.append(son**2)
-#    
-#    print(sonlar)
-#    print(sonlar_kvadrati)
+#sonlar = list(range(111)) # 0 dan 110 gacha bo'lgan sonlar ro'yxati
+#sonlar_kvadrati = [] #sonlar_kvadrati nomli bo'sh ro'yxat
+#for son in sonlar: # sonlar ro'yxatidagi xar bir son uchun
+#    sonlar_kvadrati.append(son**2) #sonlar_kvadrati ro'yxatiga qo'shish
+    
+#print(sonlar)
+#print(sonlar_kvadrati)
 
 
-#dostlar = []
-#print("5 ta eng yaqin dostingiz kim?")
-#for n in range(5):
-#    dostlar.append(input(f"{n+1}-dostingizni ismini kiriting:"))
-#print(dostlar)
 
-###################### Kamida 5 elementdan iborat ismlar degan ro'yxat tuzing, 
+
+#friends = [] #friends nomli bo'sh ro'yxat
+#print("who are your 5 best friends?") #print yordamida savol
+#for n in range(5): # 0 dan 4 gacha sonlar uchun 
+#    friends.append(input(f"{n+1}-friend:")) # friends bo'sh ro'yxatiga input 
+#    #yordamida qiymatlar qo'shish
+#print(friends)
+
+
+
+
+
+
+
+
+
+
+
+# Kamida 5 elementdan iborat ismlar degan ro'yxat tuzing, 
 # va ro'yxatdagi har bir ismga takrorlanuvchi xabar yozing
-#ismlar = ['Ali','Vali','Hasan','Husan','Olim']
-#for ism in ismlar:
-#    print(f"Assalom alaykum, {ism}. Sahifamizga xush kelibsiz!")
+
+
+#names = ['sherzod','ikromkhon','murodulla','davronbek','mukhammadrizo' ,'islam']
+#for name in names :
+#    print(f"Assalom alaykum, {name}. nonni pulini to\'lab qo\'ying!")
+
 
 # Yuoqirdagi tsikl tugaganidan so'ng, 
 # ekranga "Kod n marta takrorlandi" degan xabar chiqaring 
 # (n o'rniga kod necha marta takrorlanganini yozing)
-#print(f"Kod {len(ismlar)} marta takrorlandi")
+
+
+
+
+#print(f"Kod {len(names)} marta takrorlandi")
 
 # 10 dan 100 gacha bo'lgan toq sonlar ro'yxatini tuzing. 
 # Ro'yxatning xar bir elementining kubini yangi qatordan konsolga chiqaring.
+
 #sonlar = list(range(11,100,2))
 #for son in sonlar:
 #    print(son**3)
 
 # Foydalanuvchidan 5 ta eng sevimli kinolarini kiritshni so'rang,
 # va kinolar degan ro'yxatga saqlab oling. Natijani konsolga chiqaring.
+
+
 #kinolar = []
 #print("5 ta sevimli kinoingiz qaysilar?")
 #for k in range(5):
 #    kinolar.append(input(f"{k+1}-kino:"))
 #print(kinolar)    
 
+
+
+
 # Foydalanuvchidan bugun nechta odam bilan 
 # uchrashganini (suhbatlashganini) so'rang, 
 # va har bir suhbatlashgan odamning ismini birma-bir so'rab ro'yxatga yozing. Ro'yxatni konsolga chiqaring.
+
+
+
 #n_people = int(input("Bugun necha kishi bn suhbat qildingiz?>>>"))
 #ismlar = []
 #for n in range(n_people):
 #    ismlar.append(input(f"{n+1}-suhbat qilgan odamingiz kim edi: "))
 #print(ismlar)
+
+ 
+
+
+
 
 
 
