13b088044b804b6da5343882479849ec91719fda
otabekrazzoqov <razzoqovotabek1@gmail.com>
2023-03-03 21:37:09 +0900

    03.03.2023

--- hello-world.py
+++ hello-world.py
@@ -5790,7 +5790,6 @@
         
 #******************************************************************************
 
-       
 
 
 
@@ -5798,11 +5797,207 @@
 
 
 
+#***************************** DARS 34 ********************
+#                               JSON
+
+#import json
+
+#x = 10
+#x_json = json.dumps(x)      #dumps funksiyasi qiymatni json formatiga o'tkazadi
+                            # json formatida qiymat string shaklida bo'ladi
+#y = 5.5
+#y_json = json.dumps(y)      #json modulidagi dumps funksiyasi 
+
+#m = True
+#m_json = json.dumps(m)
+
+
+#ism = "anvar"
+#ism_json = json.dumps(ism)
+#familiya_json = json.dumps('narzullaev')
+
+#sonlar = (12, 45, 23, 67)                  #tuple bolgan O'zgarmas ro'yxat ham 
+#sonlar_json = json.dumps(sonlar)           #json da string ko'rinishida bo'ladi
+
+
+#bemor = {
+#  "ism": "Alijon Valiyev",
+#  "yosh": 30,
+#  "oila": True,
+#  "farzandlar": ("Ahmad","Bonu"),
+#  "allergiya": None,
+#  "dorilar": [
+#    {"nomi": "Panadol", "miqdori": 1.2}
+#  ]
+#}
 
 
 
 
+#bemor_json = json.dumps(bemor, indent=5)   # bemor nomli lug'atni json 
+#print(bemor_json)                          # ko'rinishiga o'tkazamiz
 
+#with open('sonlar.json','w') as f:         # json o'zgaruvchini fayl 
+#    json.dump(sonlar,f)                    # shaklida saqlash dump yordamida
+
+#bemor2 = json.loads(bemor_json)            # loads funksiyasi json formatidagi
+                                           # faylni python faylga aylantiradi
+
+#******************************************************************************
+
+# Ushbu o'zgaruvchini JSON ko'rinishida saqlang va JSON matnini 
+# konsolga chiqaring: 
+
+    
+#import json
+    
+#data = {"Model" : "Malibu", "Rang" : "Qora", "Yil":2020, "Narh":40000}
+#data_json = json.dumps(data)
+#print(data_json)
+#ith open('data.json', 'w') as f:          # data nomli lug'atni json shaklida 
+#    json.dump(data, f)                     # file qilib saqlash
+
+    
+#******************************************************************************
+
+#Ushbu JSON matnni ko'chirib oling, va talabaning ismi va familiyasini  
+#konsolga chiqaring: 
+    
+    
+#talaba_json = """{"ism":"Hasan","familiya":"Husanov","tyil":2000}"""
+#talaba = json.loads(talaba_json)           # json moduli loads funksiyasi 
+#print(talaba)                              # yordamida json faylni python
+#with open('talaba.json','w') as f:          # lug'at shakliga o'tkaziladi
+#    json.dump(talaba, f)
+
+
+#Yuqoridagi ikki o'zgaruvchini alohida JSON fayllarga saqlandi
+
+                                       
+#******************************************************************************
+
+
+
+# Quyidagi JSON faylni yuklab oling. Faylda 3 ta talabaning ism va familiyasi 
+# saqlangan. Ularning har birini alohida qatordan "Ism Familiya, n-kurs, 
+# Fakultet talabasi" ko'rinishida konsolga chiqaring.
+
+#students = {"student": [{"id": "01", "name": "Tom", "lastname": "Price", "year": 4, "faculty": "Engineering"}, {"id": "02", "name": "Nick", "lastname": "Thameson", "year": 3, "faculty": "Computer Science"}, {"id": "03", "name": "John", "lastname": "Doe", "year": 2, "faculty": "ICT"}]}
+#students_json = json.dumps(students, indent=5)
+#print(students_json)
+#print(students['student'][0])
+
+# 4 Students
+#with open('students.json') as f:
+#    data = json.load(f)
+
+#for item in data['student']:
+#    print(f"{item['name']} {item['lastname']}."
+#          f" Faculty of {item['faculty']} ")
+
+#******************************************************************************
+
+#wiki = {"batchcomplete":"","query":{"pages":{"13801":{"pageid":13801,"ns":0,"title":"Python","extract":"Python ([\u02c8p\u028c\u026a\u03b8 (\u0259)n] \u2014 payton, piton) \u2014 turli sohalar uchun yuqori darajadagi umumiy maqsadli dasturlash tili. Uning dizayn falsafasi muhim chekinishdan foydalangan holda kodning o\u02bbqilishiga urg\u02bbu beradi. Uning til konstruksiyalari va obyektga yo\u02bbnaltirilgan yondashuvi dasturchilarga kichik va yirik loyihalar uchun aniq, mantiqiy kod yozishda yordam berishga qaratilgan. Shuningdek Python sun\u02bciy intellekt hamda ma\u02bclumotlar muhandisiligi sohalarining tili hisoblanadi.\nPython deyarli barcha platformalarda ishlay oladi, xususan Windows, Linux, Mac OS X, Palm OS, Mac OS va boshqalar shular jumlasidandir. Python Microsoft.NET platformasi uchun yozilgan realizatsiyasi ham mavjud bo\u02bblib, uning nomi \u2014 IronPython dasturlash muhitidir.\nGuido van Rossum 1980-yillarning oxirida ABC dasturlash tilining davomchisi sifatida Python ustida ishlay boshladi va birinchi marta 1991-yilda Python 0.9.0 versiyasini ommaga e\u02bclon qildi.\nPython dasturlash tiliga bo\u02bblgan talab yildan yilga oshib bormoqda. CodingDojo portalining tadqiqotlariga ko\u02bbra, 2020\u20142021-yillarda aynan Python tilida dasturlovchi mutaxassislarga eng ko\u02bbp talab bo\u02bblgan."}}}}
+#wiki_json = json.dumps(wiki, indent=5)
+#print(wiki_json)
+#print(wiki.keys())
+#with open('wiki.json','w') as f:          # lug'at shakliga o'tkaziladi
+#    json.dump(wiki, f)
+
+
+
+
+#with open('wiki.json') as f:
+#    wiki = json.load(f)
+
+#print(wiki['query']['pages']['13801']['title'])
+#print(wiki['query']['pages']['13801']['extract'])
+
+
+#******************************************************************************
+
+
+#***************************** DARS 35 ********************
+#                             TRY_EXCEPT
+
+#while True:
+#    yosh = input("yoshingizni kiriting: ")
+#    try:
+#        yosh = int(yosh)
+#        print(f"Siz {2023-yosh} yilda tug'ilgansiz")    #infinite loop
+#    except:
+#        print('Siz butun son kiritmadingiz!') 
+        
+#******************************************************************************
+
+#yosh = input('yoshingizni kiriting: ')
+
+#try:
+#    yosh = int(yosh)
+#except ValueError:  #ValueError yuz bergan holda
+#    print("siz butun son kiritmadingiz:")
+#else:
+#    print(f"Siz {2023-yosh} yilda tug'ilgansiz")
+
+#print("dastur davom etyapti")
+#print("dastur tugadi")
+ 
+
+#******************************************************************************
+#ZeroDivisionError
+
+#x,y = 5,10
+#try:
+#    y/(x-5)
+#except:
+#    print(" 0 ga bo'lish mumkin emas")  
+    
+#******************************************************************************
+#IndexError
+
+#mevalar = ['olma','anor','anjir','uzum']
+#try:
+#    print(mevalar[4])
+#except IndexError:
+#    print(f"Ro'yxatda mavjud mevalar soni: {len(mevalar)}") 
+    
+#******************************************************************************
+#KeyError
+
+#user = {"username": "sariqdev",
+#        "status":"admin",
+#        "email":"sariqdev@gmail.com",
+#       "phone": "1234567"}
+
+#key = "username"
+#try:
+#    print(f"Foydalanuvchi: {user[key]}")
+#except KeyError:
+#   print("bunday kalit mavjud emas")
+    
+#******************************************************************************    
+#FileNotFoundError
+
+filename = "date.txt"  #bunday fayl mavjud emas
+
+try:
+    with open(filename) as f:
+        text = f.read()
+except FileNotFoundError:
+    print(f"{filename} mavjud emas")
+    
+#******************************************************************************
+
+import json
+files = ['talaba1.json','talaba2.json','talaba3.json','talaba4.json']
+for filename in files:
+     try:
+         with open(filename) as f:
+             talaba = json.load(f)        
+     except FileNotFoundError:
+         pass
+     else:
+         print(talaba['ism'])
 
 
 
